edeffd3c67af0224d85cea8a4ca2ed56232152f5
V Lireesh <v.lireesh@ibm.com>
2026-03-22 13:13:11 +0530

    Model trainer

--- HousePricePrediction/src/utils.py
+++ HousePricePrediction/src/utils.py
@@ -18,3 +18,19 @@ def save_object(file_path,obj):
     except Exception as e:
         logging.info("Exception occired in the initiate_datatransformation")
         raise customException(e,sys)
+    
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            #y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+        return report
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise customException(e,sys)
